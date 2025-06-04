eac0e96ec0e92306eb5ca1b317fdcf0c4fb7999e
Darkmindy <stefania.deliso1@gmail.com>
2025-06-05 00:49:37 +0200

    fix: Aggiunto 'assetsInclude' a vite.config per gestione immagini.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,7 +10,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
- 
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp'],
+
   build: {
     outDir: 'dist',
   },
